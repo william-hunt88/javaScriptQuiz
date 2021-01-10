0ccdc3db6771d8e17aae7dc21d3a245cca11a50a
w.hunt <wfh2d88@gmail.com>
2021-01-10 12:13:12 -0600

    Button Works

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// variables defining spaces to be clicked and syuch
+var startBtn = document.querySelector('#start-btn');
+console.log(startBtn);
+
+
+
+
+
+
+// Start button is clicked and opening description disappears
+
+// timer starts and 1st question is revealed
+
+
+
+
+
+
+var firstQuestion = function () {
+    console.log("boobs")
+}
+
+
+
+
+
+
+
+
+
+// Event Listeners
+startBtn.addEventListener("click" , firstQuestion);
+
+
+
+
+
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers - GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
